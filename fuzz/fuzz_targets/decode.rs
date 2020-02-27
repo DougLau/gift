93df4915f7f0d290e55ba396c691769be8ce2b36
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-02-27 12:44:30 +0100

    Add a target for cargo fuzz.

---
+++ fuzz/fuzz_targets/decode.rs
@@ -0,0 +1,16 @@
+// cargo fuzz run decode corpus/decode -- -timeout=30
+
+#![no_main]
+
+use std::io::Cursor;
+use libfuzzer_sys::fuzz_target;
+
+use gift::Decoder;
+
+fuzz_target!(|data: &[u8]| {
+    for frame in Decoder::new_unbuffered(Cursor::new(data)) {
+        if frame.is_err() {
+            return;
+        }
+    }
+});
